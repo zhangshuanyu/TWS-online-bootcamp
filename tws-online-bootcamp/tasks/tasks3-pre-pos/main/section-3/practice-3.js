b28523544cb86836b065dcf775aa37a2127da493
zhangshuanyu <849125536@qq.com>
2017-08-01 17:41:36 +0800

    第五次提交

---
+++ tws-online-bootcamp/tasks/tasks3-pre-pos/main/section-3/practice-3.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+module.exports = function createUpdatedCollection(collectionA, objectB) {
+  var count=1;
+  var collectionC=[];
+  var n=0;
+ 
+  for(var i;i<collectionA.length;i++){
+      if(collectionA[i]===collectionA[i+1]){
+          count++;
+      }else{
+          collectionC[n]= { key:collectionA[i],count: count};
+          count = 1;
+          n++;        
+      }
+  }    
+for(var C in collectionC){
+      for(var B in objectB.value){
+          if(collectionC[C].key===objectB.value[B]){
+              collectionC[C].count-=Math.floor((collectionC[C].count)/3);
+          }
+      }
+      
+  }
+  return collectionC;
+
+}
