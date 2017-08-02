04fcfc51b289e5e0c0d0758c9054ca3426f28682
zhangshuanyu <849125536@qq.com>
2017-08-02 12:09:55 +0800

    测试提交！

--- tws-online-bootcamp/tasks/tasks3-pre-pos/main/section-2/practice-3.js
+++ tws-online-bootcamp/tasks/tasks3-pre-pos/main/section-2/practice-3.js
@@ -1,5 +1,63 @@
 'use strict';
 
+function find(collection, ch) {
+  for(let item of collection){
+    if (item.name === ch){
+      return item;
+    }
+  }
+  return null;
+    
+}
+function summarize(collection) {
+  let result =[];
+  for(let item of collection){
+    let obj = find(result,item);
+    if(obj){
+      obj.summary++;
+    }else {
+      result.push({name:item,summary:1});
+    }
+    return result;
+
+  }
+    
+}
+function split(item) {
+  if(item.includes("-")) {
+        let array = item.split("-");
+        return {key: array[0], count: parseInt(array[1], 10)};
+  }
+  if(item.includes("[")) {
+        let key = item.charAt(0);
+        let count = parseInt(item.substr(2,item.length - 1));
+        return {key, count};
+  }
+  if(item.includes(":")) {
+        let array = item.split(":");
+        return {key: array[0], count: parseInt(array[1], 10)};
+  }
+}
+function push(result,key,count){
+  for(var i=0;i<count;i++){
+    result.push(key);
+  }
+}
+function expand(collection) {
+  let result=[];
+  for (let item of collection){
+    if(item.length === 1){
+      result.push(item);
+    }else {
+      let {key,count}=split(item);
+      push(result,key,count);
+    }
+    return result;
+  }
+    
+}
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let  expandedArray = expand(collection);
+  return summarize(expandedArray);
+
 }
--- tws-online-bootcamp/tasks/tasks3-pre-pos/main/section-3/practice-2.js
+++ tws-online-bootcamp/tasks/tasks3-pre-pos/main/section-3/practice-2.js
@@ -4,10 +4,12 @@ module.exports = function createUpdatedCollection(collectionA, objectB) {
     for(var A in collectionA){
         for(var B in objectB.value){
             if(collectionA[A].key===objectB.value[B]){
-                collectionA[A].count -= 1;
-                
+                collectionA[A].count -= Math.floor((collectionA[A].count)/3);
+
+
             }
         }
     }
-  return collectionA;
+    return collectionA;
 }
+
